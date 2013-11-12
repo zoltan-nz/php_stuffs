113fcdc156b9b8ff637db28405e9fe1f87e7774a
Zoltan <zdebre@gmail.com>
2013-11-12 22:25:03 +0000

    add test migration in memory to TestCase

--- laravel_practice/app/tests/TestCase.php
+++ laravel_practice/app/tests/TestCase.php
@@ -3,6 +3,17 @@
 class TestCase extends Illuminate\Foundation\Testing\TestCase {
 
 	/**
+	 * Default preparation for each test
+	 */
+
+    public function setUp ()
+    {
+        parent::setUp();
+
+        $this->prepareForTests();
+    }
+
+    /**
 	 * Creates the application.
 	 *
 	 * @return Symfony\Component\HttpKernel\HttpKernelInterface
@@ -15,5 +26,13 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase {
 
 		return require __DIR__.'/../../bootstrap/start.php';
 	}
+
+    /** Migrate the database
+     */
+    private function prepareForTests()
+    {
+        Artisan::call('migrate');
+    }
+
 
 }
