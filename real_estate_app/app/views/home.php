f0cbb1e939b7b25b7dea3a5a45ecfe2109f09dbb
Zoltan <zdebre@gmail.com>
2013-12-03 01:10:18 +0000

    Add asset pipeline to Laravel

---
+++ real_estate_app/app/views/home.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Real Estate App</title>
+    <?= stylesheet_link_tag() ?>
+    <?= javascript_include_tag() ?>
+</head>
+<body>
+
+</body>
+</html>
